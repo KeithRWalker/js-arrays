9047d05d28df3277e201c4b4c459bd5f9f5e6cc7
Keith Walker <Keithrwalker418@gmail.com>
2019-03-05 20:29:01 -0600

    added dinosaur array/loop/domprint and css styling

--- main.js
+++ main.js
@@ -1,1 +1,50 @@
-console.log('js-arrays main.js linked');
+//loop over dinos
+//build up a domstring
+//print domstring to the dom
+
+const dinosaurs = [
+    {
+        dinoType: 'Tyrannosaurus',
+        dinoSize: '40ft long, 15-25ft tall',
+        dinoDiet: 'Carnivore',
+        dinoPic:  './imgs/trex.jpg'
+    },
+    {
+        dinoType: 'Velociraptor',
+        dinoSize: '10ft',
+        dinoDiet: 'Carnivore',
+        dinoPic: './imgs/raptor.jpg'
+    },
+    {
+        dinoType: 'Allosaurus',
+        dinoSize: '30ft',
+        dinoDiet: 'Carnivore',
+        dinoPic: './imgs/allo.jpg' 
+    }
+];
+
+const print = (id, message, br) => {
+    const selectedDiv = document.getElementById(id);
+    selectedDiv.innerHTML = message;
+    if(br){selectedDiv.innerHTML +='<br>'};
+  };
+
+const buildDinosaurs = () => {
+    let domString = '';
+    for(let i=0; i<dinosaurs.length; i++){
+        domString += `<div class="cardDiv">`;
+        domString +=    `<h1 class="dinoName">${dinosaurs[i].dinoType}</h1>`;
+        domString +=    `<img class="dinoPic" src= "${dinosaurs[i].dinoPic}">`;
+        domString +=    `<p class="dinoSize">SIZE: ${dinosaurs[i].dinoSize}</p>`;
+        domString +=    `<p class="dinoDiet">DIET: ${dinosaurs[i].dinoDiet}</p>`;
+        domString += `</div>`;
+    }
+    print('dinoDiv', domString, false);
+};
+
+  const init = () => {
+    buildDinosaurs();
+  };
+
+  init();
+  console.log('testing');
